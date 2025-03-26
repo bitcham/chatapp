1d1e302ce596c01cd1712b808a66053da7b403e5
Chambit Oh <chambit.oh@gmail.com>
2025-03-26 21:32:01 +0200

    start

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+
+
+const io = new Server(server);
+
+
+
+
+
